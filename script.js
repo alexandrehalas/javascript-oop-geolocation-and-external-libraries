ea927e6997fc5cfddb452d4b2da7b1bc4e48a8cf
Alexandre Halas <alexandrehalas@gmail.com>
2023-09-22 17:38:12 -0300

    Using the Geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+const geolocationSuccess = function (position) {
+  const { latitude } = position.coords;
+  const { longitude } = position.coords;
+  console.log(latitude, longitude);
+  console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+};
+const geolocationError = function () {
+  alert('Could not get your position');
+};
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    geolocationSuccess,
+    geolocationError
+  );
